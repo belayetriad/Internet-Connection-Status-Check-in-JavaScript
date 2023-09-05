29b44a500b5b9fb44dbc26305e90f85ccb45452d
Belayet Hossen <belayet@bhalogari.com>
2023-09-06 00:07:36 +0600

    Initial Commit

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-Swal.fire({
-    title: 'Internet Connected',
-    text: 'You are connected to the internet.',
-    icon: 'success',
-    timer: 3000, // Set the timer to 3000 milliseconds (3 seconds)
+// Function to show internet connection status
+function showInternetConnectionStatus() {
+    if (navigator.onLine) {
+        Swal.fire({
+            title: 'Internet Connected',
+            text: 'You are connected to the internet.',
+            icon: 'success',
+            timer: 3000, // Set the timer to 3000 milliseconds (3 seconds)
     showConfirmButton: false, // Hide the "OK" button
-}).then((result) => {
-    // This code will be executed when the popup is closed
-    if (result.dismiss === Swal.DismissReason.timer) {
-        // The popup was closed by the timer
-        console.log('Popup closed by timer');
+        });
+    } else {
+        Swal.fire({
+            title: 'Internet Disconnected',
+            text: 'You are not connected to the internet.',
+            icon: 'error',
+            timer: 3000, // Set the timer to 3000 milliseconds (3 seconds) 
+            showConfirmButton: false, // Hide the "OK" button
+        });
     }
-});
+}
+
+// Initial check when the page loads
+showInternetConnectionStatus();
+
+// Listen for online and offline events
+window.addEventListener('online', showInternetConnectionStatus);
+window.addEventListener('offline', showInternetConnectionStatus);
